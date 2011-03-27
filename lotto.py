bfae3f9332f8824c97126df8281482d7345287f4
Simos Xenitellis <simos@gnome.org>
2011-03-27 15:27:53 +0300

    Υπολογισμός κερδών για 100000 στήλες· υποστήριξη για πέντε+ένα

--- lotto.py
+++ lotto.py
@@ -13,7 +13,9 @@ def getLottoNumbers():
         numbers.append(candidate)
     numbers.sort()
 
-    return numbers
+    special = random.randint(1, 49)
+
+    return numbers, special
 
 def checkWinningNumbers(winning, mynumbers):
   common = []
@@ -24,8 +26,32 @@ def checkWinningNumbers(winning, mynumbers):
   return common
 
 winning_numbers = [3, 13, 14, 41, 43, 49]
+winning_special = 20
 
-my_numbers = getLottoNumbers()
-result = checkWinningNumbers(winning_numbers, my_numbers)
-print "Πιάσατε", result
+stiles = 100000
+exari = 0
+pentariena = 0
+pentari = 0
+pentari = 0
+tessari = 0
+triari = 0
 
+
+for i in range(0, stiles):
+  mynumbers, myspecial = getLottoNumbers()
+  result = checkWinningNumbers(winning_numbers, mynumbers)
+  if len(result) == 6:
+    exari = exari + 1
+  elif len(result) == 5 and myspecial == winning_special:
+    pentariena = pentariena + 1
+  elif len(result) == 5 and myspecial != winning_special:
+    pentari = pentari + 1
+  elif len(result) == 4:
+    tessari = tessari + 1
+  elif len(result) == 3:
+    triari = triari + 1
+
+winnings = exari * 1400000 + pentariena * 50000 + pentari * 1500 + tessari * 30 + triari * 1.5
+expense = stiles * 0.5
+
+print "Ξοδέψατε", expense, "ευρώ και κερδίσατε", winnings
